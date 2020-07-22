02251802c1a59ed7d27261bbd545de4868929813
egor_istomin <witto24@gmail.com>
2020-07-22 22:50:27 +0500

    first commit

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./post-list.css";
+
+import PostListItem from "../post-list-item";
+
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
+  const elements = posts.map((item)=> {
+  const {id, ...itemProps} = item;
+return (
+  <li key={id} className='list-group-item'>
+    <PostListItem {...itemProps}
+    onDelete={() => onDelete(id)}
+    onToggleImportant={() => onToggleImportant(id)}
+    onToggleLiked={() => onToggleLiked(id)}/>
+  </li>
+ )
+});
+
+
+  return (
+    <ul className="app-list list-group">
+    {elements}
+    </ul>
+
+  )
+}
+
+export default PostList;
